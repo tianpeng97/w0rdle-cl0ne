7bbae683a06dd503e54d1ce2525be3f6265adca6
Tian Peng <ianpeng97@hotmail.com>
2022-03-17 07:38:17 -0400

    chore: initial commit

---
+++ README.old.md
@@ -0,0 +1,1 @@
+# w0rdle-cl0ne
---
+++ src/App.js
@@ -0,0 +1,15 @@
+import "./App.css";
+
+function App() {
+    return (
+        <div className="App">
+            <nav>
+                <h1>Wordle</h1>
+            </nav>
+            <Board />
+            <Keyboard />
+        </div>
+    );
+}
+
+export default App;
