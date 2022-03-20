e8af7f1384ea5f4f92d315237151d4f6ff524bb7
Tian Peng <ianpeng97@hotmail.com>
2022-03-20 07:39:52 -0400

    feat: add keyboard and digital keyboard support

--- src/App.js
+++ src/App.js
@@ -1,15 +1,69 @@
 import "./App.css";
+import Board from "./components/Board";
+import Keyboard from "./components/Keyboard";
+import { boardDefault } from "./components/Words";
+import { createContext, useState } from "react";
+
+export const AppContext = createContext();
 
 function App() {
-    return (
-        <div className="App">
-            <nav>
-                <h1>Wordle</h1>
-            </nav>
-            <Board />
-            <Keyboard />
+  const [board, setBoard] = useState(boardDefault);
+  const [currentAttempt, setCurrentAttempt] = useState({
+    attempt: 0,
+    letterPos: 0,
+  });
+
+  const onSelectLetter = (keyVal) => {
+    if (currentAttempt.letterPos > 4) return;
+    const newBoard = [...board];
+    newBoard[currentAttempt.attempt][currentAttempt.letterPos] = keyVal;
+    setBoard(newBoard);
+    setCurrentAttempt({
+      ...currentAttempt,
+      letterPos: currentAttempt.letterPos + 1,
+    });
+  };
+  const onDelete = () => {
+    if (currentAttempt.letterPos === 0) return;
+    const newBoard = [...board];
+    newBoard[currentAttempt.attempt][currentAttempt.letterPos - 1] = "";
+    setBoard(newBoard);
+    setCurrentAttempt({
+      ...currentAttempt,
+      letterPos: currentAttempt.letterPos - 1,
+    });
+  };
+  const onEnter = () => {
+    if (currentAttempt.letterPos !== 5) return;
+    setCurrentAttempt({
+      attempt: currentAttempt.attempt + 1,
+      letterPos: 0,
+    });
+  };
+
+  return (
+    <div className="App">
+      <nav>
+        <h1>Wordle</h1>
+      </nav>
+      <AppContext.Provider
+        value={{
+          board,
+          setBoard,
+          currentAttempt,
+          setCurrentAttempt,
+          onSelectLetter,
+          onDelete,
+          onEnter,
+        }}
+      >
+        <div className="game">
+          <Board />
+          <Keyboard />
         </div>
-    );
+      </AppContext.Provider>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/Words.jsx
@@ -0,0 +1,4 @@
+// 6x5 matrix
+export const boardDefault = Array(6)
+    .fill()
+    .map(() => Array(5).fill(""));
